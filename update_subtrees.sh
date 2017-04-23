32e323ce245147ab80861d6e9fdd901e18f73a82
Jason Yan <tailofthesun@gmail.com>
2017-04-23 16:40:32 -0700

    Added duviz.

--- update_subtrees.sh
+++ update_subtrees.sh
@@ -3,5 +3,6 @@
 git subtree pull --squash --prefix usr/contrib/autojump https://github.com/wting/autojump.git master
 git subtree pull --squash --prefix usr/contrib/base16-shell https://github.com/chriskempson/base16-shell.git master
 git subtree pull --squash --prefix usr/contrib/dircolors-solarized https://github.com/seebi/dircolors-solarized master
+git subtree pull --squash --prefix usr/contrib/duviz https://github.com/soxofaan/duviz master
 git subtree pull --squash --prefix usr/contrib/transcrypt https://github.com/elasticdog/transcrypt master
 git subtree pull --squash --prefix .tmux/plugins/tmux-yank https://github.com/tmux-plugins/tmux-yank.git master

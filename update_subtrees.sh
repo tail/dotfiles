8b5415af05d2a11571d53d15e06c33bee422a41b
Jason Yan <tailofthesun@gmail.com>
2022-01-26 22:29:59 -0800

    add git-filter-repo

--- update_subtrees.sh
+++ update_subtrees.sh
@@ -6,3 +6,4 @@ git subtree pull --squash --prefix usr/contrib/dircolors-solarized https://githu
 git subtree pull --squash --prefix usr/contrib/duviz https://github.com/soxofaan/duviz master
 git subtree pull --squash --prefix usr/contrib/transcrypt https://github.com/elasticdog/transcrypt master
 git subtree pull --squash --prefix .tmux/plugins/tmux-yank https://github.com/tmux-plugins/tmux-yank.git master
+git subtree pull --squash --prefix usr/contrib/git-filter-repo https://github.com/newren/git-filter-repo main
